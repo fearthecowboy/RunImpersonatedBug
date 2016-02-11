fb2d1e4d6a13d579e848e98d5bd419b53c5c9f71
Garrett Serack <garretts@microsoft.com>
2016-02-11 09:53:05 -0800

    Bug: Works on dnxcore50 not on net46

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            Action action = () => {
+                Console.WriteLine($"I am {WindowsIdentity.GetCurrent().Name}!");
+            };
+            
+            var id = WindowsIdentity.GetCurrent() as WindowsIdentity;
+            
+            WindowsIdentity.RunImpersonated(id.AccessToken, action);
+        }
+    }
+}
